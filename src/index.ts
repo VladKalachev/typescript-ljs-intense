50bc85caf9dc5c4feac68ae9c336c3e94e08c72c
Corvax <vlvete1990@yandex.ru>
2020-06-03 19:49:29 +0300

    adding webpack and webpack.config.js

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+function sleep(ms: number) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    })
+}
+
+async function* getItemsReallySlowly<T>(items: Iterable<T>): AsyncIterableIterator<T> {
+    for (const item of items) {
+        await sleep(1000);
+        yield item;
+    }
+}
+
+export async function speakLikeSloth(items: string[]) {
+    for await (const item of getItemsReallySlowly(items)) {
+        console.log(item);
+    }
+}
+
+speakLikeSloth(['hi', 'all', 'TS', 'is', 'Awesome']);
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'none',
+    entry: './src',
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, 'dist')
+    },
+    devtool: 'source-map',
+    resolve: {
+        extensions: ['.ts', '.js', '.json']
+    },
+    module: {
+        rules: [
+            {test: /\.ts$/, loader: 'ts-loader'},
+            {test: /\.css$/, use: ['style-loader', 'css-loader']},
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        })
+    ]
+}
